093b980ae9210587522ab6172ef30d4359737dc1
Rugved <spamrugved@gmail.com>
2026-07-03 11:28:00 +0530

    Add: kth-largest-element-in-an-array - submission-4

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-4.py
@@ -0,0 +1,32 @@
+import random
+from typing import List
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        target = len(nums) - k
+        left, right = 0, len(nums) - 1
+
+        while True:
+            pivot = nums[random.randint(left, right)]
+
+            lt = left
+            i = left
+            gt = right
+
+            while i <= gt:
+                if nums[i] < pivot:
+                    nums[lt], nums[i] = nums[i], nums[lt]
+                    lt += 1
+                    i += 1
+                elif nums[i] > pivot:
+                    nums[gt], nums[i] = nums[i], nums[gt]
+                    gt -= 1
+                else:
+                    i += 1
+
+            if target < lt:
+                right = lt - 1
+            elif target > gt:
+                left = gt + 1
+            else:
+                return pivot
